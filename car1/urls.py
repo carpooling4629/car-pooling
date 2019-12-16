7f160505f16046ad238bce80ce2c70c2c7dce850
Deeshath <48383269+luciferztd@users.noreply.github.com>
2019-12-16 21:19:33 +0530

    hi

---
+++ car1/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path, include
+from . import views
+urlpatterns = [
+    path(r'', views.signin),
+    path(r'signin/', views.signin),
+    path(r'signup/',views.signup),
+    path(r'update_profile/',views.update),
+    path(r'forgot_password/',views.forgot_password),
+    path(r'signout/',views.signout),
+    path(r'home/',views.home),
+    path(r'home/lift/', views.lift),
+    path(r'home/trip/', views.trip),
+path(r'firebase/', views.firebase1),
+    path(r'home/cancelLift/',views.cancel_ride_request),
+    path(r'home/acceptTrip/',views.accept_lift_request)
+]
